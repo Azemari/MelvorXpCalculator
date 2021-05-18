4f1728977ae6945031dc28d4145eec22610b9fb7
Azemari <sam.bennett1998@hotmail.com>
2021-05-18 09:53:10 +0100

    Smithing user data now persists

--- MelvorXpCalculator/Controllers/SkillsController.cs
+++ MelvorXpCalculator/Controllers/SkillsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MelvorXpCalculator.Models;
 using MelvorXpCalculator.Utilities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MelvorXpCalculator.Controllers
@@ -12,24 +13,66 @@ namespace MelvorXpCalculator.Controllers
     {
         public IActionResult Smithing(GlobalViewModel globalViewModel)
         {
-            if(globalViewModel.ModifiersBonus == 0)
+            Skill skill = new()
+            {
+                Name = "Smithing",
+                ImageUrl = "/Images/Skills/Smithing.svg"
+            };
+
+            //If we have consent, get the data
+            if (Request.Cookies["Consent"] == "true")
             {
                 try
                 {
-                    globalViewModel.ModifiersBonus = int.Parse(Request.Cookies["totalBonus"]);
+                    skill.Level = int.Parse(Request.Cookies["SmithingLevel"]);
+                    skill.Xp = float.Parse(Request.Cookies["SmithingXp"]);
+                    skill.TargetLevel = int.Parse(Request.Cookies["SmithingTargetLevel"]);
+                    skill.TargetXp = float.Parse(Request.Cookies["SmithingTargetXp"]);
                 }
-                catch { }
+                catch {
+                    skill.Level = 1;
+                    skill.Xp = 1;
+                    skill.TargetLevel = 1;
+                    skill.TargetXp = 1;
+                }
             }
+            else
+            {
+                skill.Level = 1;
+                skill.Xp = 1;
+                skill.TargetLevel = 1;
+                skill.TargetXp = 1;
+            }
+
+            if (int.TryParse(Request.Cookies["totalBonus"], out int totalBonus))
+                globalViewModel.ModifiersBonus = totalBonus;
 
 
             SkillViewModel viewModel = new()
             {
                 ModifierBonus = globalViewModel.ModifiersBonus,
-                Skill = Data.RetrieveSkill("Smithing"),
+                Skill = skill,
                 Items = Data.RetrieveItems("Smithing")
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Smithing(SkillViewModel form)
+        {
+            form.Items = Data.RetrieveItems("Smithing");
+            //Save new levels to cookie
+            CookieOptions options = new();
+            options.Expires = DateTime.Now.AddYears(1);
+
+            //Get each modifier and save them to the cookies (for retrieval) and update the GlobalViewModel
+            Response.Cookies.Append("SmithingLevel", form.Skill.Level.ToString(), options);
+            Response.Cookies.Append("SmithingXp", form.Skill.Xp.ToString(), options);
+            Response.Cookies.Append("SmithingTargetLevel", form.Skill.TargetLevel.ToString(), options);
+            Response.Cookies.Append("SmithingTargetXp", form.Skill.TargetXp.ToString(), options);
+
+            return View(form);
         }
 
         public IActionResult Cooking(GlobalViewModel globalViewModel)
--- MelvorXpCalculator/Models/Skill.cs
+++ MelvorXpCalculator/Models/Skill.cs
@@ -3,10 +3,10 @@
     public class Skill
     {
         public string Name { get; set; }
-        public string Category { get; set; }
-        public string SubCatergory { get; set; }
         public string ImageUrl { get; set; }
         public float Xp { get; set; }
         public int Level { get; set; }
+        public float TargetXp { get; set; }
+        public int TargetLevel { get; set; }
     }
 }
